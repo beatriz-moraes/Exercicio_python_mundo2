7d6824af82baeedb98e8bba746560c896424da77
Beatriz Moraes <bia_moraes16@hotmail.com>
2020-05-12 23:10:14 -0300

    Exercicio 57 mundo2

---
+++ ex57_validacao_de_dados.py
@@ -0,0 +1,11 @@
+'''Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores
+'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.'''
+
+sexo = str(input('Digite seu sexo: [M/F]: ')).strip().upper()[0]
+while True:
+    if sexo != 'M' and sexo != 'F':
+        sexo = str(input('Dados inválidos! Por favor, digite seu sexo [M/F]: ')).strip().upper()[0]
+    else:
+        print(f'Sexo {sexo} registrado com sucesso!')
+        break
+
